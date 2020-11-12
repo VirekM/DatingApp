e7d1a3528bfbbaf78ac7084369f382035cd0bd24
Virek <virek.maharaj@prosense.co.za>
2020-11-12 13:30:02 +0200

    End of section 8

---
+++ API/DTO/PhotoDto.cs
@@ -0,0 +1,11 @@
+namespace API.DTO
+{
+    public class PhotoDto
+    {
+        public int id { get; set; }
+
+        public string Url { get; set; }
+
+        public bool isMain { get; set; }
+    }
+}
